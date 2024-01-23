9bc06c480ae45f3a8d0b87e50d7bd196b509dc30
David Thorpe <djt@mutablelogic.com>
2024-01-23 11:13:03 +0100

    Updates

--- examples/grafana.tf
+++ examples/grafana.tf
@@ -6,14 +6,14 @@ module "grafana" {
   // Required parameters  
   dc             = local.datacenter             // Nomad datacenter for the cluster
   namespace      = local.namespace              // Nomad namespace for the cluster
-  hosts          = ["cm2"]                      // Host constraint for the job
   admin_password = local.GRAFANA_ADMIN_PASSWORD // Password for the admin user
 
   // Optional parameters
   enabled     = true                 // If false, no-op
+  hosts       = ["server1"]          // Host constraint for the job. If not specified, the job will be deployed to one node
   docker_tag  = "latest"             // Pull the latest version of the docker image every job restart
   port        = 3000                 // Port to expose
-  data        = "/var/lib/influxdb"  // Data persistence directory
+  data        = "/var/lib/influxdb"  // Data persistence directory. If not set, then data is not persisted
   admin_email = "admin@mutablelogic" // Email address for the admin user
   anonymous   = false                // When true, allow anonymous access as a viewer
 }
