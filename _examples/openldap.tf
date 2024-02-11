ca7865941e1a55e8db1b8b72072d271a788ba47c
David Thorpe <djt@mutablelogic.com>
2024-02-11 12:03:25 +0100

    Added some documentation

--- _examples/openldap.tf
+++ _examples/openldap.tf
@@ -1,5 +1,5 @@
 
-// Example LDAP server, using the bitnami/openldap docker image
+// Example LDAP server
 module "openldap" {
   source = "github.com/mutablelogic/tf-nomad//openldap"
 
@@ -18,4 +18,27 @@ module "openldap" {
   port              = 389                                            // plaintext port to expose
   replication_hosts = ["ldap://server1:389/", "ldap://server2:389/"] // LDAP urls for replication
   data              = "/var/lib/ldap"                                // Directory for data persistence
+}
+
+// Example LDAP admin
+module "openldap-admin" {
+  source =  "github.com/mutablelogic/tf-nomad//openldap-admin"
+
+  // Required parameters
+  dc             = var.dc
+  hosts           = ["server1", "server2"]    // Host constraint for the job
+  organization    = "My Organization"         // Distinquished name for the LDAP server
+  domain          = "example.com"             // Domain for the LDAP server
+  admin_password  = local.LDAP_ADMIN_PASSWORD // Password for the LDAP 'admin' user
+
+  // Other parameters
+  enabled        = true
+  namespace      = var.namespace
+  docker_tag        = "latest"                                       // Pull the latest version of the docker image every job restart
+  port              = 5000                                           // plaintext port to expose
+
+  // LDAP details
+  service_dns    = ["192.168.86.11", "192.168.86.12"]
+  url            = "ldap://openldap-ldap.default.nomad/"
+  basedn         = "dc=mutablelogic,dc=com"
 }
