6d7ba12859df687498b79be24c88353acd63b39a
David Thorpe <djt@mutablelogic.com>
2024-02-11 10:49:08 +0100

    Propogate the debug flag

--- coredns/main.tf
+++ coredns/main.tf
@@ -8,20 +8,21 @@ resource "nomad_job" "coredns" {
     vars = {
       dc                 = jsonencode([var.dc])
       namespace          = var.namespace
-      hosts              = jsonencode(var.hosts)      
+      hosts              = jsonencode(var.hosts)
       docker_image       = local.docker_image
       docker_always_pull = jsonencode(local.docker_always_pull)
       service_provider   = var.service_provider
       service_name       = var.service_name
       service_dns        = jsonencode(var.service_dns)
       service_type       = var.service_type
-      
-      port               = var.port
-      corefile           = file("${path.module}/config/Corefile")
-      nomad_addr         = var.nomad_addr
-      nomad_token        = var.nomad_token
-      cache_ttl          = var.cache_ttl
-      dns_zone           = var.dns_zone
+      debug              = var.debug
+
+      port        = var.port
+      corefile    = file("${path.module}/config/Corefile")
+      nomad_addr  = var.nomad_addr
+      nomad_token = var.nomad_token
+      cache_ttl   = var.cache_ttl
+      dns_zone    = var.dns_zone
     }
   }
 }
