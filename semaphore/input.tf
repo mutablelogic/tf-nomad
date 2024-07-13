9e27a0a82da6532a5befd078b3c92ac2cf993be3
David Thorpe <djt@mutablelogic.com>
2024-07-13 12:47:45 +0200

    Updated semaphore

--- semaphore/input.tf
+++ semaphore/input.tf
@@ -16,6 +16,24 @@ variable "enabled" {
   default     = true
 }
 
+variable "service_provider" {
+  description = "Service provider, either consul or nomad"
+  type        = string
+  default     = "nomad"
+}
+
+variable "service_name" {
+  description = "Service name"
+  type        = string
+  default     = "semaphore"
+}
+
+variable "service_dns" {
+  description = "Service discovery DNS"
+  type        = list(string)
+  default     = []
+}
+
 variable "docker_tag" {
   type        = string
   description = "Version of the docker image to use, defaults to latest"
@@ -24,7 +42,8 @@ variable "docker_tag" {
 
 variable "hosts" {
   type        = list(string)
-  description = "List of hosts to deploy on (required)"
+  description = "List of hosts to deploy on"
+  default     = []
 }
 
 variable "port" {
@@ -33,14 +52,57 @@ variable "port" {
   default     = 3000
 }
 
-variable "data" {
+variable "admin_user" {
+  description = "Admin user"
   type        = string
-  description = "Directory for data persistence"
-  default     = ""
+  default     = "Admin"
 }
 
 variable "admin_password" {
   description = "Admin password (required)"
+  type        = string
+  sensitive   = true
+}
+
+variable "db" {
+  description = "Database parameters"
+  type = object({
+    type = string // postgres, mysql
+    host = string
+    port = number
+    name = string
+    user = string
+  })
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+  sensitive   = true
+}
+
+variable "ldap" {
+  description = "LDAP parameters"
+  type = object({
+    host          = string
+    port          = number
+    tls           = optional(bool)
+    dn_bind       = string
+    dn_search     = string
+    filter_search = string
+  })
+  default = {
+    host          = ""
+    port          = 389
+    tls           = false
+    dn_bind       = ""
+    dn_search     = ""
+    filter_search = ""
+  }
+}
+
+variable "ldap_password" {
+  description = "LDAP password"
   type        = string
   sensitive   = true
 }
--- semaphore/main.tf
+++ semaphore/main.tf
@@ -8,12 +8,28 @@ resource "nomad_job" "semaphore" {
     vars = {
       dc                 = jsonencode([var.dc])
       namespace          = var.namespace
+      service_provider   = var.service_provider
+      service_name       = var.service_name
+      service_dns        = jsonencode(var.service_dns)
       docker_image       = local.docker_image
       docker_always_pull = jsonencode(local.docker_always_pull)
       hosts              = jsonencode(var.hosts)
       port               = var.port
-      data               = var.data
+      admin_user         = var.admin_user
       admin_password     = var.admin_password
+      db_type            = var.db.type
+      db_host            = var.db.host
+      db_port            = var.db.port
+      db_name            = var.db.name
+      db_user            = var.db.user
+      db_password        = var.db_password
+      ldap_host          = var.ldap.host
+      ldap_port          = var.ldap.port
+      ldap_tls           = var.ldap.tls
+      ldap_dn_bind       = var.ldap.dn_bind
+      ldap_password      = var.ldap_password
+      ldap_dn_search     = var.ldap.dn_search
+      ldap_filter_search = var.ldap.filter_search
     }
   }
 }
