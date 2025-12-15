1b4ef765732cc70d87ee163fee05f8fb5501bea4
David Thorpe <djt@mutablelogic.com>
2025-12-15 21:15:46 +0100

    Update pulling of tags

--- postgres/locals.tf
+++ postgres/locals.tf
@@ -1,5 +1,5 @@
 
 locals {
-    docker_image = "ghcr.io/mutablelogic/docker-postgres:${var.docker_tag}"
-    docker_always_pull = false
+  docker_image       = "ghcr.io/mutablelogic/docker-postgres:${var.docker_tag}"
+  docker_always_pull = true
 }
